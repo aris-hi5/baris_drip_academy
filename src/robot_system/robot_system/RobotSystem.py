f13de24de34f7c5acb4af373651d5292011e7fdb
jangjh0201 <jangjh0201@gmail.com>
2024-07-29 18:53:46 +0900

    request_cnt 추가

--- src/robot_system/robot_system/RobotSystem.py
+++ src/robot_system/robot_system/RobotSystem.py
@@ -4,65 +4,74 @@ from library.Constants import Command, RobotParameter, Constants, RobotCommand
 from message.srv import RobotService
 import datetime
 
+
 class RobotSystem:
     """
-        로봇 시스템 클래스
+    로봇 시스템 클래스
     """
-    QU5TV0VSX0NNRA = [RobotCommand.HOME_NORMAL,
-                  RobotCommand.GRIPPER_INIT,
-                  RobotCommand.PICKUP,
-                  RobotCommand.HOLD,
-                  RobotCommand.UNHOLD,
-                  RobotCommand.HOME_NORMAL,
-                  RobotCommand.FLATTENING,
-                  RobotCommand.PLACE_DRIP,
-                  RobotCommand.PICKUP,
-                  RobotCommand.PLACE_CUP,
-                  RobotCommand.PICKUP,
-                  RobotCommand.DRAIN_FIT,
-                  RobotCommand.HOME_KETTLE,
-                  RobotCommand.DRIP,
-                  RobotCommand.HOME_KETTLE,
-                  RobotCommand.DRAIN_ALL,
-                  RobotCommand.PLACE,
-                  RobotCommand.HOME_NORMAL,
-                  RobotCommand.PICKUP_CUP,
-                  RobotCommand.PLACE,
-                  RobotCommand.PICKUP_DRIP,
-                  RobotCommand.PLACE,
-                  RobotCommand.GESTURE]
 
-    QU5TV0VSX1BBUkFNRVRFUg = [RobotParameter.ZERO,
-                        RobotParameter.ZERO,
-                        RobotParameter.DSP,
-                        RobotParameter.COF,
-                        RobotParameter.COF,
-                        RobotParameter.ZERO,
-                        RobotParameter.ZON,
-                        RobotParameter.ZON,
-                        RobotParameter.DSP,
-                        RobotParameter.ZON,
-                        RobotParameter.KET,
-                        RobotParameter.HOT,
-                        RobotParameter.ZERO,
-                        RobotParameter.DPO,
-                        RobotParameter.ZERO,
-                        RobotParameter.ZERO,
-                        RobotParameter.KET,
-                        RobotParameter.ZERO,
-                        RobotParameter.ZON,
-                        RobotParameter.PIC,
-                        RobotParameter.ZON,
-                        RobotParameter.BIN,
-                        RobotParameter.ETC]
+    QU5TV0VSX0NNRA = [
+        RobotCommand.HOME_NORMAL,
+        RobotCommand.GRIPPER_INIT,
+        RobotCommand.PICKUP,
+        RobotCommand.HOLD,
+        RobotCommand.UNHOLD,
+        RobotCommand.HOME_NORMAL,
+        RobotCommand.FLATTENING,
+        RobotCommand.PLACE_DRIP,
+        RobotCommand.PICKUP,
+        RobotCommand.PLACE_CUP,
+        RobotCommand.PICKUP,
+        RobotCommand.DRAIN_FIT,
+        RobotCommand.HOME_KETTLE,
+        RobotCommand.DRIP,
+        RobotCommand.HOME_KETTLE,
+        RobotCommand.DRAIN_ALL,
+        RobotCommand.PLACE,
+        RobotCommand.HOME_NORMAL,
+        RobotCommand.PICKUP_CUP,
+        RobotCommand.PLACE,
+        RobotCommand.PICKUP_DRIP,
+        RobotCommand.PLACE,
+        RobotCommand.GESTURE,
+    ]
+
+    QU5TV0VSX1BBUkFNRVRFUg = [
+        RobotParameter.ZERO,
+        RobotParameter.ZERO,
+        RobotParameter.DSP,
+        RobotParameter.COF,
+        RobotParameter.COF,
+        RobotParameter.ZERO,
+        RobotParameter.ZON,
+        RobotParameter.ZON,
+        RobotParameter.DSP,
+        RobotParameter.ZON,
+        RobotParameter.KET,
+        RobotParameter.HOT,
+        RobotParameter.ZERO,
+        RobotParameter.DPO,
+        RobotParameter.ZERO,
+        RobotParameter.ZERO,
+        RobotParameter.KET,
+        RobotParameter.ZERO,
+        RobotParameter.ZON,
+        RobotParameter.PIC,
+        RobotParameter.ZON,
+        RobotParameter.BIN,
+        RobotParameter.ETC,
+    ]
 
     def __init__(self):
 
         # 로봇 객체
         self.robot = None
         self.before_pos = "cmd"
-        self.request_pos= None
-        self.e23b713133da90cea5a333838bba1bc3fde020ea8a6314ad8579b7ca12334bb7 = Constants.ZERO
+        self.request_pos = None
+        self.request_cnt = 0
+        self.e23b713133da90cea5a333838bba1bc3fde020ea8a6314ad8579b7ca12334bb7 = (
+            Constants.ZERO
+        )
 
     def get_cur_cmd(self):
         return self.before_pos
@@ -71,7 +80,14 @@ class RobotSystem:
         response = None
         response = RobotService.Response()
         try:
-            request_list = [request.cmd, request.par1, request.par2, request.par3, request.par4, request.par5]
+            request_list = [
+                request.cmd,
+                request.par1,
+                request.par2,
+                request.par3,
+                request.par4,
+                request.par5,
+            ]
             response.component_cd = DeviceCode.ROBOT
             response.seq_no = str(datetime.datetime.now())
             if request.cmd == Command.RESET:
@@ -80,10 +96,19 @@ class RobotSystem:
                 response.response_cd = ResponseCode.SUCCESS
                 response.result = "RESET SEQUENCE"
 
-            elif request.cmd == RobotSystem.QU5TV0VSX0NNRA[self.e23b713133da90cea5a333838bba1bc3fde020ea8a6314ad8579b7ca12334bb7] and request.par1 == RobotSystem.QU5TV0VSX1BBUkFNRVRFUg[self.e23b713133da90cea5a333838bba1bc3fde020ea8a6314ad8579b7ca12334bb7]:
+            elif (
+                request.cmd
+                == RobotSystem.QU5TV0VSX0NNRA[
+                    self.e23b713133da90cea5a333838bba1bc3fde020ea8a6314ad8579b7ca12334bb7
+                ]
+                and request.par1
+                == RobotSystem.QU5TV0VSX1BBUkFNRVRFUg[
+                    self.e23b713133da90cea5a333838bba1bc3fde020ea8a6314ad8579b7ca12334bb7
+                ]
+            ):
                 self.request_cnt += 1
                 self.before_pos = request.cmd
-                if request.cmd == RobotCommand.HOME :
+                if request.cmd == RobotCommand.HOME:
                     response.status_cd = DeviceStatus.STANDBY
                     response.response_cd = ResponseCode.SUCCESS
                     response.result = self.make_str(request_list)
@@ -187,11 +212,11 @@ class RobotSystem:
                     response.status_cd = DeviceStatus.RESET
                     response.response_cd = ResponseCode.SUCCESS
                     response.result = "Reset all commands"
-                else :
+                else:
                     response.status_cd = DeviceStatus.ERROR
                     response.response_cd = ResponseCode.ERROR
                     response.result = "CMD Unable"
-            else :
+            else:
                 response.status_cd = DeviceStatus.ERROR
                 response.response_cd = ResponseCode.ERROR
                 response.result = "CMD Conflict, Try Another CMD"
@@ -201,22 +226,22 @@ class RobotSystem:
 
         finally:
             return response
-        
+
     def make_str(self, result_list):
         result_str = ""
-        split_char = '//'
+        split_char = "//"
         result_str = ""
         try:
             for i in range(len(result_list)):
                 result_str = result_str + result_list[i]
-                if i < len(result_list)-1:
+                if i < len(result_list) - 1:
                     result_str += split_char
         except Exception as error:
             print(f"RobotSystem make_str ERROR {error}")
             result_str = "ERROR"
-        finally: 
-            return result_str 
+        finally:
+            return result_str
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass

5a67aa46bc90b838350d14760ad82dfd9f7e6b94
wjstndlr <jbm689772@google.com>
2024-07-31 12:28:48 +0900

    Mod : 통신 성공

--- src/operation_system/operation_system/client.py
+++ src/operation_system/operation_system/client.py
@@ -1,11 +1,11 @@
 import sqlite3
 import rclpy
 from rclpy.node import Node
-from message.srv import RobotService, DispenseService
+from message.srv import RobotService, DispenseService, DBService
 from message.msg import DispenserStatus
 from library.Constants import Service, Topic
 
-class RobotDispenserClient(Node):
+class Client(Node):
 
     def __init__(self):
         super().__init__('robot_dispenser_client')
@@ -19,6 +19,10 @@ class RobotDispenserClient(Node):
         self.dispense_client = self.create_client(DispenseService, Service.SERVICE_DISPENSER)
         while not self.dispense_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('Waiting for DispenseService...')
+        
+        self.db_client = self.create_client(DBService, Service.SERVICE_DB)
+        while not self.db_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info('Waiting for DBService...')
         
         # DispenserStatus 토픽 구독자 생성
         self.subscription = self.create_subscription(
@@ -56,32 +60,39 @@ class RobotDispenserClient(Node):
         for component in msg.component:
             self.get_logger().info(f'Component status: {component.status}, stock: {component.stock}, status_code: {component.status_code}')
     
-    
+
+    def send_DB_request(self, seq_no):
+        request = DBService.Request()
+        request.seq_no = seq_no
+       
+        self.future = self.db_client.call_async(request)
+        rclpy.spin_until_future_complete(self, self.future)
+        return self.future.result()
 
 
-    
-def fetch_commands_from_db(db_path):
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-    cursor.execute("SELECT seq_no, command, par1, par2, par3, par4, par5 FROM commands ORDER BY seq_no")
-    commands = cursor.fetchall()
-    conn.close()
-    return commands
+# def fetch_commands_from_db(db_path):
+#     conn = sqlite3.connect(db_path)
+#     cursor = conn.cursor()
+#     cursor.execute("SELECT seq_no, command, par1, par2, par3, par4, par5 FROM commands ORDER BY seq_no")
+#     commands = cursor.fetchall()
+#     conn.close()
+#     return commands
 
 def main(args=None):
     rclpy.init(args=args)
-    client = RobotDispenserClient()
+    client = Client()
 
-    db_path = 'path_to_your_database.db'
-    commands = fetch_commands_from_db(db_path)
+    # db_path = 'path_to_your_database.db'
+    # commands = fetch_commands_from_db(db_path)
+    for i in range(1,26):
+        response = client.send_DB_request(str(i))
+        commands = [response.cmd, response.par1, response.par2, response.par3, response.par4, response.par5]
 
-    for cmd in commands:
-        seq_no, command, par1, par2, par3, par4, par5 = cmd
-        if command in ['COFFEE_ON', 'WATER_TOGGLE']:
-            response = client.send_dispense_request(seq_no, command)
+        if commands[0] in ['COFFEE_ON', 'WATER_TOGGLE']:
+            response = client.send_dispense_request(str(i), commands[0])
         else:
-            response = client.send_robot_request(seq_no, command, par1, par2, par3, par4, par5)
-        client.get_logger().info(f'Response for {command}: {response}')
+            response = client.send_robot_request(str(i), commands[0], commands[1], commands[2], commands[3], commands[4], commands[5])
+        client.get_logger().info(f'Response for {commands}: {response}')
 
     client.destroy_node()
     rclpy.shutdown()
